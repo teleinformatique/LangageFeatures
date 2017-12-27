0af8ae74bed1ca76d79094882ec340a200893997
Khadim Sarr <khadimsarr@outlook.com>
2017-12-27 11:06:58 +0000

    Using automaticaly implemented property

--- LangageFeatures/Models/Product.cs
+++ LangageFeatures/Models/Product.cs
@@ -8,13 +8,15 @@ namespace LangageFeatures.Models
     public class Product
     {
         public string Name { get; set; }
+        public string Category { get; set; } = "Watersports";
         public decimal? Price { get; set; }
         public Product Related { get; set; }
+        public bool InStock { get; } = true;
 
 
         public static Product[] GetProducts()
         {
-            Product kayak = new Product { Name = "Kayak", Price = 275M };
+            Product kayak = new Product { Name = "Kayak", Price = 275M, Category = "Water Craft" };
             Product lifejacket = new Product { Name = "Lifejacket", Price = 48.95M };
             kayak.Related = lifejacket;
 
