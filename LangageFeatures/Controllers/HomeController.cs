c92de4b80cb2423b8e0c24ca165e89e11a83ab88
Khadim Sarr <khadimsarr@outlook.com>
2017-12-27 10:53:59 +0000

    used the null conditional operator

--- LangageFeatures/Controllers/HomeController.cs
+++ LangageFeatures/Controllers/HomeController.cs
@@ -2,6 +2,7 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using LangageFeatures.Models;
 using Microsoft.AspNetCore.Mvc;
 
 namespace LangageFeatures.Controllers
@@ -10,7 +11,17 @@ namespace LangageFeatures.Controllers
     {
         public ViewResult Index()
         {
-            return View(new string[] { "C#", "Language", "Features"});
+            List<string> results = new List<string>();
+
+            foreach (Product p in Product.GetProducts())
+            {
+                string name = p?.Name ?? "<No name>";
+                decimal? price = p?.Price ?? 0;
+                string relatedName = p?.Related?.Name ?? "<None>";
+
+                results.Add(string.Format("Name: {0}, Price: {1}, related: {2}", name, price, relatedName));
+            }
+            return View(results);
         }
     }
 }
--- LangageFeatures/Models/Product.cs
+++ LangageFeatures/Models/Product.cs
@@ -9,11 +9,14 @@ namespace LangageFeatures.Models
     {
         public string Name { get; set; }
         public decimal? Price { get; set; }
+        public Product Related { get; set; }
 
-        public static Product[] GetProduct()
+
+        public static Product[] GetProducts()
         {
             Product kayak = new Product { Name = "Kayak", Price = 275M };
             Product lifejacket = new Product { Name = "Lifejacket", Price = 48.95M };
+            kayak.Related = lifejacket;
 
             return new Product[] { kayak, lifejacket, null };
         }
