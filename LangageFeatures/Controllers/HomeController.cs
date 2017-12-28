c549fd85f18d7b5dd7ed84eee47bd01500f30956
Khadim Sarr <khadimsarr@outlook.com>
2017-12-28 11:01:41 +0000

    Using ExtensionMethods

--- LangageFeatures/Controllers/HomeController.cs
+++ LangageFeatures/Controllers/HomeController.cs
@@ -33,13 +33,33 @@ namespace LangageFeatures.Controllers
             decimal total = 0;
             for (int i = 0; i < data.Length; i++)
             {
-                if (data[i] is decimal d)
+                //if (data[i] is decimal d)
+                //{
+                //    total += d;
+                //}
+                switch (data[i])
                 {
-                    total += d;
+                    case decimal decimalValue:
+                        total += decimalValue;
+                        break;
+
+                    case int intValue when intValue > 50:
+                        total += intValue;
+                        break;
                 }
             }
 
-            return View(new string [] { $"Total: {total:C2}" });
+            //ExtensionMethods
+            ShopingCart cart = new ShopingCart { Products = Product.GetProducts() };
+            decimal cartTotal = cart.TotalPrices();
+            Product[] productArray = {
+            new Product {Name = "Kayak", Price = 275M},
+            new Product {Name = "Lifejacket", Price = 48.95M},
+            new Product {Name = "Soccer ball", Price = 19.50M},
+            new Product {Name = "Corner flag", Price = 34.95M}
+            };
+            decimal arrayTotal = productArray.FilterByPrice(20).TotalPrices();
+            return View(new string [] { $"Total: {cartTotal:C2} TotalArray: {arrayTotal:C2}" });
         }
     }
 }
